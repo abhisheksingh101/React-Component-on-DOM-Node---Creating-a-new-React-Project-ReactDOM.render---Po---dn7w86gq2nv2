819717d450ecaa6a9a746e33676e06fd9a1c1333
Abhishek Singh <70930933+abhisheksingh101@users.noreply.github.com>
2022-11-13 00:11:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,3 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-const App = () => {
-  return (
 
-<div id="root"><p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    }
-}
-    
-export default App;
+<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+
